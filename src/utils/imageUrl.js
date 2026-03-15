8acf753f1b32ffee9a6c1d9cbbbfd4fe9755ca6b
nagesh-roy <nageshkuppala06@gmail.com>
2026-03-15 18:53:38 +0530

    urls change

--- src/utils/imageUrl.js
+++ src/utils/imageUrl.js
@@ -1,10 +1,10 @@
-const BASE_URL = "http://localhost:8001"
+const BASE_URL = "https://storyhub-backend-1.onrender.com"
 
 export const getImageUrl = (path) => {
   if (!path) return null
 
   // if already full URL
-  if (path.startsWith("http")) {
+  if (path.startsWith("http") || path.startsWith("https")) {
     return path
   }
 
